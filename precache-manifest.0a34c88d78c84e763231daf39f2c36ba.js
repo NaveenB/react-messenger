8f4dd0069e1f3d340b8fcfe056f8b4a0da26f8b4
NaveenB <naveen.bandhu@gmail.com>
2020-06-18 01:30:21 +0200

    Updates

---
+++ precache-manifest.0a34c88d78c84e763231daf39f2c36ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb49dd821e23066eaf0",
+    "url": "naveenb.github.io/react-messenger/static/css/main.0eded730.chunk.css"
+  },
+  {
+    "revision": "4bb49dd821e23066eaf0",
+    "url": "naveenb.github.io/react-messenger/static/js/main.4bb49dd8.chunk.js"
+  },
+  {
+    "revision": "e247fdc41a6ecaab8aa7",
+    "url": "naveenb.github.io/react-messenger/static/js/1.e247fdc4.chunk.js"
+  },
+  {
+    "revision": "354fdca55d0e8263a0cb",
+    "url": "naveenb.github.io/react-messenger/static/js/runtime~main.354fdca5.js"
+  },
+  {
+    "revision": "2444c79da4c0d0b9c4f495d959ae3574",
+    "url": "naveenb.github.io/react-messenger/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "naveenb.github.io/react-messenger/precache-manifest.0a34c88d78c84e763231daf39f2c36ba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("naveenb.github.io/react-messenger/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
